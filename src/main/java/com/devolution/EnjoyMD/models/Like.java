a351c647f195b7abf8169a248e3f916764c1b20b
MaslovMaxim <mmaslov1470@mail.ru>
2024-08-26 14:49:04 +0300

    added all models

---
+++ src/main/java/com/devolution/EnjoyMD/models/Like.java
@@ -0,0 +1,31 @@
+package com.devolution.EnjoyMD.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Data
+@Table(name = "likes")
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
