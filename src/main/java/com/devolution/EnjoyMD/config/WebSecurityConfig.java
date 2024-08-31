a6dfb58faa1b79e2d9e3eeed98950e4e2cecc762
Alexandros <alexandr.papas@mail.ru>
2024-08-31 17:04:47 +0300

    main-auth

--- src/main/java/com/devolution/EnjoyMD/config/WebSecurityConfig.java
+++ src/main/java/com/devolution/EnjoyMD/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/","/css/**", "/img/**", "/fonts/**","/register", "/user").permitAll()
+                        .requestMatchers("/","/css/**", "/img/**", "/fonts/**","/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
